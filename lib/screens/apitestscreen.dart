3896a618780450d96c28e92d81140dbe9918aa02
ManiMazaheri <mani.mazaheri.dev@outlook.com>
2023-11-20 13:09:06 +0330

    edit

--- lib/screens/apitestscreen.dart
+++ lib/screens/apitestscreen.dart
@@ -1,19 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<void> fetchData() async {
-  final response =
-      await http.get(Uri.parse('https://apicms.ir/api/v1/Auth/Captcha'));
-
-  if (response.statusCode == 200) {
-    // Parse the response data
-    print('Response data: ${response.body}');
-  } else {
-    // Handle errors
-    print('Error: ${response.statusCode}');
-  }
-}
-
 class ApiService {
   final String baseUrl;
 
